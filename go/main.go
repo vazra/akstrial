a4d72e13e207f0ef362e073cb365fc1357081772
Ameen Rashad <ameenrashad@hotmail.com>
2020-09-26 14:07:12 +0530

    Add go sample

---
+++ go/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"math"
+	"strconv"
+  "net/http"
+  "github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/prime", func(c *gin.Context) {
+		paramNo := c.Query("no") // shortcut for c.Request.URL.Query().Get("lastname")
+		theNo, err := strconv.Atoi(paramNo)
+    if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"no":paramNo, "error":err.Error() } )
+    		return
+		}
+		
+		var cal = math.Pow(10, float64(theNo))
+		primes := sieveOfEratosthenes(int(cal))	
+    c.JSON(http.StatusOK, gin.H{"no":theNo, "primes":len(primes),"time":0 })    
+	})
+	router.Run(":8080")
+}
+
+func sieveOfEratosthenes(N int) (primes []int) {
+	b := make([]bool, N)
+	for i := 2; i < N; i++ {
+			if b[i] == true { continue }
+			primes = append(primes, i)
+			for k := i * i; k < N; k += i {
+					b[k] = true
+			}
+	}
+	return
+}
